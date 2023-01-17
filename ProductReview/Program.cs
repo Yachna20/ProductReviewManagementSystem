6210606fbef66fd14c6be26fbd33e1c6d154f060
Yachna20 <thakreyachna20@gmail.com>
2023-01-17 21:47:09 +0530

    [Yachna]-RetrieveAllRecordFromListwhoseProductIdis1or4or9andRating>3

--- ProductReview/Program.cs
+++ ProductReview/Program.cs
@@ -41,7 +41,7 @@ namespace ProductReview
         new Product{ProductID = 25, Userid = 25, Rating = 4, Review = "Nice", IsLike = true},
         };
 
-         ProductManagement.Top3Records(product);
+         ProductManagement.RetrieveRecords(product);
 
     
       }
